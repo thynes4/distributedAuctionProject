734804cf2e843fce481d7b91da7437b9346ff60a
Thomas Hynes <tjhynes4@unm.edu>
2023-05-09 17:12:01 -0600

    Completed our MessageListener classes and got most of the template set for our MessageParser classes just need to handle each different message instance that each class will face

--- src/bank/MessageParser.java
+++ src/bank/MessageParser.java
@@ -1,4 +1,42 @@
 package bank;
 
-public class MessageParser {
+import general.Message;
+import javafx.util.Pair;
+import general.Message;
+import general.Message.*;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.BlockingQueue;
+
+public class MessageParser implements Runnable {
+    private final Bank bank;
+    private final BlockingQueue<Pair<Message, ObjectOutputStream>> messages;
+    private boolean loop;
+
+    protected MessageParser(Bank bankObj, BlockingQueue<Pair<Message, ObjectOutputStream>> messages) {
+        this.bank = bankObj;
+        this.messages = messages;
+        loop = true;
+    }
+
+    protected void stop() {
+        loop = false;
+    }
+
+    @Override
+    public void run() {
+        while (loop) {
+            try {
+
+                Pair<Message, ObjectOutputStream> data = messages.take();
+                ObjectOutputStream out = data.getValue();
+                Message m = data.getKey();
+
+                //ADD FUNCTIONALITY TO HANDLE DIFFERENT MESSAGES
+
+            } catch (InterruptedException e) {
+                System.out.println("Error: Message parser interrupted: " + e.getMessage());
+            }
+        }
+    }
+
 }
