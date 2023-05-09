44f7cf0d663b60851e426b5a5881f6ed89a58f70
Thomas Hynes <tjhynes4@unm.edu>
2023-05-09 17:41:05 -0600

    Added template for SocketParsers

--- src/auctionHouse/SocketParser.java
+++ src/auctionHouse/SocketParser.java
@@ -1,4 +1,29 @@
 package auctionHouse;
 
-public class SocketParser {
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+public class SocketParser implements Runnable {
+    private boolean loop;
+    private final AuctionHouse auctionHouse;
+    private final BlockingQueue<Socket> sockets;
+
+    protected SocketParser(AuctionHouse ahObj, BlockingQueue<Socket> sockObj) {
+        this.auctionHouse = ahObj;
+        this.sockets = sockObj;
+        loop = true;
+    }
+
+    public void stop () {
+        loop = false;
+    }
+    @Override
+    public void run() {
+        while (loop) {
+
+
+
+        }
+    }
 }
+
--- src/bank/SocketParser.java
+++ src/bank/SocketParser.java
@@ -1,4 +1,28 @@
 package bank;
 
-public class SocketParser {
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+public class SocketParser implements Runnable {
+    private boolean loop;
+    private final Bank bank;
+    private final BlockingQueue<Socket> sockets;
+
+    protected SocketParser(Bank bankObj, BlockingQueue<Socket> sockObj) {
+        this.bank = bankObj;
+        this.sockets = sockObj;
+        loop = true;
+    }
+
+    public void stop () {
+        loop = false;
+    }
+    @Override
+    public void run() {
+        while (loop) {
+
+
+
+        }
+    }
 }
