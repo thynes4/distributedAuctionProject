e5a335433a5f96d12519a3c84453511d2ce55838
Thomas Hynes <tjhynes4@unm.edu>
2023-05-09 09:58:13 -0600

    Added general files for how we should communicate our data

---
+++ src/general/Message.java
@@ -0,0 +1,31 @@
+/**
+ * Thomas Hynes, Christopher Jarek, Carmen Manohan
+ * Interface to be used to format our messages between the bank, agent,
+ * and auction house
+ */
+
+package general;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public interface Message {
+    record AgentMade(String accNum) implements Message, Serializable {}
+    record newAgent(String name, double startBal) implements Message, Serializable {}
+    record RegisterAgent(String accNum, String name) implements Message, Serializable {}
+    record NewHold(String accNum, double amount, String accAndID, int idNum) implements Message, Serializable {}
+    record ConfirmHold(boolean success, String item, String accNum, int id) implements Message, Serializable {}
+    record EndHold(String accNum, double amount, String item) implements Message, Serializable {}
+    record closeAgent(String accNum) implements Message, Serializable {}
+    record AuctionList(Map<String, SocketData> houses) implements Message, Serializable {}
+    record AuctionHouseMade(String accNum) implements Message, Serializable {}
+    record NewAuctionHouse(String name, int port) implements Message, Serializable {}
+    record NewAuctions(String name, List<AuctionData> auctionListings) implements Message, Serializable {}
+    record AuctionOver(String  ahNum, String clientNum, String item, double amount) implements Message, Serializable {}
+    record AuctionWon(String item, double amount) implements Message, Serializable {}
+    record AuctionHouseClosed(String accNum) implements Message, Serializable {}
+    record ConfirmBid(boolean success, String item, String name) implements Message, Serializable {}
+    record updateMoney(double amount, double hold) implements Message, Serializable {}
+
+}
